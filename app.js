2ed353e374c9f1da560fd1e210da41733a776197
Cazetto-RS <rafaeletecbosta@gmail.com>
2025-02-18 08:01:35 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    return res.status(200).json();
+});
+
+const port = 3000;
+app.listen(port,()=>{
+    console,log(`Aplicação está em execução\n http://localhost:${port}`)
+});
